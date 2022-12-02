275954f3d7e73f846fd554e5cb6907e4b8f34ec1
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-12-02 11:46:50 +0000

    reset password

--- 0x00-vagrant/README.md
+++
@@ -1,4 +0,0 @@
-# Testing the Installed Vagrant
-
-run ```uname``` on your terminal to test the Vagrant installed properly
-The result is saved in [0-hello_ubuntu](https://github.com/jacobgbemi/zero_day/blob/master/0x00-vagrant/0-hello_ubuntu)
--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-import smtplib
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
@@ -14,15 +13,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-mail_settings = {
-    "MAIL_SERVER": 'smtp.gmail.com',
-    "MAIL_PORT": 465,
-    "MAIL_USE_TLS": False,
-    "MAIL_USE_SSL": True,
-    "MAIL_USERNAME": os.environ.get('EMAIL_USER'),
-    "MAIL_PASSWORD": os.environ.get('EMAIL_PASSWORD')
-}
-app.config.update(mail_settings)
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 mail = Mail(app)
+
 
 from blog import routes
